79ee3b43ed5bbee6028fd30ac99cdcf960c5dd0b
Martin Björkström <martin.bjorkstrom@gmail.com>
2019-01-02 00:00:17 +0200

    (build) Use correct overload of string.TrimStart for full framework

--- src/Cake.Bakery/Arguments/ArgumentParser.cs
+++ src/Cake.Bakery/Arguments/ArgumentParser.cs
@@ -15,7 +15,7 @@ namespace Cake.Bakery.Arguments
         {
             return arguments.Select(arg =>
             {
-                var args = arg.UnQuote().TrimStart('-').Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
+                var args = arg.UnQuote().TrimStart(new[] { '-' }).Split(new[] { '=' }, 2, StringSplitOptions.RemoveEmptyEntries);
 
                 return Tuple.Create(args[0].ToLower(), args.Length < 2 ? string.Empty : args[1].UnQuote());
             }).ToDictionary(x => x.Item1, x => x.Item2);
