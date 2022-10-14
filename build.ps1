83ae24e11b6de9034f88267cf5a74c8de43fc50e
Mattias Karlsson <matkar@wcom.se>
2022-10-14 15:33:58 +0200

    (GH-174) Updated Cake tool to version 2.3.0 * fixes #174

--- build.ps1
+++ build.ps1
@@ -1,69 +1,86 @@
 #!/usr/bin/env pwsh
-$ErrorActionPreference = 'Stop'
 $DotNetInstallerUri = 'https://dot.net/v1/dotnet-install.ps1';
 $DotNetUnixInstallerUri = 'https://dot.net/v1/dotnet-install.sh'
-$DotNetChannel = 'LTS'
 $PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
 
-[string] $DotNetVersion = '6.0.100'
+# Make sure tools folder exists
+$ToolPath = Join-Path $PSScriptRoot "tools"
+if (!(Test-Path $ToolPath)) {
+    Write-Verbose "Creating tools directory..."
+    New-Item -Path $ToolPath -Type Directory -Force | out-null
+}
+
+
+if ($PSVersionTable.PSEdition -ne 'Core') {
+    # Attempt to set highest encryption available for SecurityProtocol.
+    # PowerShell will not set this by default (until maybe .NET 4.6.x). This
+    # will typically produce a message for PowerShell v2 (just an info
+    # message though)
+    try {
+        # Set TLS 1.2 (3072), then TLS 1.1 (768), then TLS 1.0 (192), finally SSL 3.0 (48)
+        # Use integers because the enumeration values for TLS 1.2 and TLS 1.1 won't
+        # exist in .NET 4.0, even though they are addressable if .NET 4.5+ is
+        # installed (.NET 4.5 is an in-place upgrade).
+        [System.Net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
+      } catch {
+        Write-Output 'Unable to set PowerShell to use TLS 1.2 and TLS 1.1 due to old .NET Framework installed. If you see underlying connection closed or trust errors, you may need to upgrade to .NET Framework 4.5+ and PowerShell v3'
+      }
+}
 
 ###########################################################################
 # INSTALL .NET CORE CLI
 ###########################################################################
 
-$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE = 1
-$env:DOTNET_CLI_TELEMETRY_OPTOUT = 1
-$env:DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX = 2
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+$env:DOTNET_CLI_TELEMETRY_OPTOUT=1
+$env:DOTNET_ROLL_FORWARD_ON_NO_CANDIDATE_FX=2
 
-Function Remove-PathVariable([string]$VariableToRemove) {
-  $SplitChar = ';'
-  if ($IsMacOS -or $IsLinux) {
-    $SplitChar = ':'
-  }
 
-  $path = [Environment]::GetEnvironmentVariable("PATH", "User")
-  if ($path -ne $null) {
-    $newItems = $path.Split($SplitChar, [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
-    [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join($SplitChar, $newItems), "User")
-  }
+Function Remove-PathVariable([string]$VariableToRemove)
+{
+    $SplitChar = ';'
+    if ($IsMacOS -or $IsLinux) {
+        $SplitChar = ':'
+    }
 
-  $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
-  if ($path -ne $null) {
-    $newItems = $path.Split($SplitChar, [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
-    [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join($SplitChar, $newItems), "Process")
-  }
+    $path = [Environment]::GetEnvironmentVariable("PATH", "User")
+    if ($path -ne $null)
+    {
+        $newItems = $path.Split($SplitChar, [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
+        [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join($SplitChar, $newItems), "User")
+    }
+
+    $path = [Environment]::GetEnvironmentVariable("PATH", "Process")
+    if ($path -ne $null)
+    {
+        $newItems = $path.Split($SplitChar, [StringSplitOptions]::RemoveEmptyEntries) | Where-Object { "$($_)" -inotlike $VariableToRemove }
+        [Environment]::SetEnvironmentVariable("PATH", [System.String]::Join($SplitChar, $newItems), "Process")
+    }
 }
 
-# Get .NET Core CLI path if installed.
-$FoundDotNetCliVersion = $null;
-if (Get-Command dotnet -ErrorAction SilentlyContinue) {
-  $FoundDotNetCliVersion = dotnet --version;
-}
-
-if ($FoundDotNetCliVersion -ne $DotNetVersion) {
-  $InstallPath = Join-Path $PSScriptRoot ".dotnet"
-  if (!(Test-Path $InstallPath)) {
+$InstallPath = Join-Path $PSScriptRoot ".dotnet"
+$GlobalJsonPath = Join-Path $PSScriptRoot "global.json"
+if (!(Test-Path $InstallPath)) {
     New-Item -Path $InstallPath -ItemType Directory -Force | Out-Null;
-  }
+}
 
-  if ($IsMacOS -or $IsLinux) {
+if ($IsMacOS -or $IsLinux) {
     $ScriptPath = Join-Path $InstallPath 'dotnet-install.sh'
     (New-Object System.Net.WebClient).DownloadFile($DotNetUnixInstallerUri, $ScriptPath);
-    & bash $ScriptPath --version "$DotNetVersion" --install-dir "$InstallPath" --channel "$DotNetChannel" --no-path
+    & bash $ScriptPath --jsonfile "$GlobalJsonPath" --install-dir "$InstallPath" --no-path
 
     Remove-PathVariable "$InstallPath"
     $env:PATH = "$($InstallPath):$env:PATH"
-  }
-  else {
+}
+else {
     $ScriptPath = Join-Path $InstallPath 'dotnet-install.ps1'
     (New-Object System.Net.WebClient).DownloadFile($DotNetInstallerUri, $ScriptPath);
-    & $ScriptPath -Channel $DotNetChannel -Version $DotNetVersion -InstallDir $InstallPath;
+    & $ScriptPath -JSonFile $GlobalJsonPath -InstallDir $InstallPath;
 
     Remove-PathVariable "$InstallPath"
     $env:PATH = "$InstallPath;$env:PATH"
-  }
-  $env:DOTNET_ROOT = $InstallPath
 }
+$env:DOTNET_ROOT=$InstallPath
 
 ###########################################################################
 # RUN BUILD SCRIPT
